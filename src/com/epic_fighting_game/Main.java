a96b7ee2cc65dac37132f482d912b7d4af91d070
tmaulon <thomas.maulon@gmail.com>
2020-01-11 12:30:06 +0100

    begin create a  new rodeur and print his attributes and his methods

--- src/com/epic_fighting_game/Main.java
+++ src/com/epic_fighting_game/Main.java
@@ -2,6 +2,16 @@ package com.epic_fighting_game;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello OpenClassrooms");
+    //Les méthodes d'un Rôdeur
+    Rodeur rodeur = new Rodeur(50, 40, 30, 20, 10);
+    System.out.println(rodeur.toString());
+
+    System.out.println("--------------------------------------------");
+    //Les méthodes de l'interface
+        rodeur.attaque_basique();
+        rodeur.attaque_speciale();
+
+    System.out.println("--------------------------------------------");
+    //Utilisons le polymorphisme de notre interface
     }
 }
