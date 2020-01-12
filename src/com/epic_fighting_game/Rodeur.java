24761ba872fb6413056886761c3a84c05d3769d7
tmaulon <thomas.maulon@gmail.com>
2020-01-12 12:46:31 +0100

    create rodeur class

--- src/com/epic_fighting_game/Rodeur.java
+++ src/com/epic_fighting_game/Rodeur.java
@@ -1,17 +1,23 @@
 package com.epic_fighting_game;
 
 public class Rodeur extends Personnage {
-    public Rodeur(int niveau, int vie, int force, int agilite, int intelligence) {
-        super(niveau, vie, force, agilite, intelligence);
+    public Rodeur(int niveau, int force, int agilite, int intelligence) {
+        super(niveau, force, agilite, intelligence);
     }
 
     @Override
-    public void attaque_basique() {
-        System.out.println("Ceci est mon attaque basique : tir à l'arc");
+    public String cri() {
+        return "Coucou";
     }
 
     @Override
-    public void attaque_speciale() {
-        System.out.println("Ceci est mon attaque basique : concentration");
+    public int attaque_basique() {
+        return this.getAgilite();
+    }
+
+    @Override
+    public int attaque_speciale() {
+        this.setAgilite( this.getAgilite() + (this.getAgilite() / 2));
+        return this.getAgilite();
     }
 }
