ce4e12c84126a4730660145a0679d525ed80c2d0
tmaulon <thomas.maulon@gmail.com>
2020-01-12 12:45:37 +0100

    rename interface

--- src/com/epic_fighting_game/IAttaques_Personnage.java
+++
@@ -1,6 +0,0 @@
-package com.epic_fighting_game;
-
-public interface IAttaques_Personnage {
-    public void attaque_basique();
-    public void attaque_speciale();
-}
---
+++ src/com/epic_fighting_game/IPersonnage.java
@@ -0,0 +1,7 @@
+package com.epic_fighting_game;
+
+public interface IPersonnage {
+    public String cri();
+    public int attaque_basique();
+    public int attaque_speciale();
+}
